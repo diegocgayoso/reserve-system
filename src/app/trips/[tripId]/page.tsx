6b1a8ca0349c3d4cfd9d522098c932232f5850cc
Diego Carlos <diegoc.gayoso@gmail.com>
2025-07-01 17:06:11 -0300

    feat: change TripdDetailsPage for use Components essencial

---
+++ src/app/trips/[tripId]/page.tsx
@@ -0,0 +1,41 @@
+import { prisma } from "@/lib/prisma";
+import TripHeader from "./components/TripHeader";
+import TripReservation from "./components/TripReservation";
+import TripDescription from "./components/TripDescription";
+import TripHighlights from "./components/TripHighlights";
+import TripLocation from "./components/TripLocation";
+
+const getTripDetails = async (tripId: string) => {
+  const trip = await prisma.trip.findUnique({
+    where: { id: tripId },
+  });
+  return trip;
+};
+
+const TripDetails = async ({ params }: { params: { tripId: string } }) => {
+  const trip = await getTripDetails(params.tripId);
+  if (!trip) return null;
+
+  return (
+    <div className="container mx-auto">
+      {/* Imagens */}
+      <TripHeader trip={trip} />
+      <div className="flex flex-col lg:flex-row lg:mt-12 lg:gap-20">
+        <TripReservation
+          tripId={trip.id}
+          tripStartDate={trip.startDate}
+          tripEndDate={trip.endDate}
+          maxGuests={trip.maxGuest}
+          pricePerDay={trip.pricePerDay as any}
+        />
+      </div>
+
+      <div className="lg:order-1">
+        <TripDescription description={trip.description} />
+        <TripHighlights highlights={trip.highlights} />
+      </div>
+      <TripLocation location={trip.location} locationDescription={""}  />
+    </div>
+  );
+};
+export default TripDetails;
