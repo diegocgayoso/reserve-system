21d1051aff16e6b831ac1af97b67c9057f542d16
Diego Carlos <diegoc.gayoso@gmail.com>
2025-07-01 17:49:03 -0300

    style: add border to tripReservation

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -34,8 +34,9 @@ const TripReservation = ({ trip }: TripReservationProps) => {
         <p className="font-medium text-sm text-roxo-dark">Total:</p>
         <p className="font-medium text-sm text-roxo-dark">R$2500</p>
       </div>
-
-      <Button className="mt-4">Reservar agora</Button>
+      <div className="pb-10 border-b border-cinza w-full">
+        <Button className="mt-4 w-full">Reservar agora</Button>
+      </div>
     </div>
   );
 };
