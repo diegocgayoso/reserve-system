bef32ca161c06e54d3d607eed91a90980b809fa8
Diego Carlos <diegoc.gayoso@gmail.com>
2025-07-01 19:20:14 -0300

    feat: create footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import { NextAuthProvider } from "@/providers/auth";
 import "./globals.css";
 import { Poppins } from "next/font/google";
-import Header from "@/components/header";
-
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"]
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
 export default function RootLayout({
@@ -18,9 +18,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={poppins.className}>
         <NextAuthProvider>
-          <Header/>
+          <Header />
           {children}
-          </NextAuthProvider>
+          <Footer />
+        </NextAuthProvider>
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return ( <div className="bg-cinza">
+        <div className="container mx-auto p-6 text-center text-cinza-claro">
+            <p className="text-sm">© 2023 System Trips. Todos os direitos reservados.</p>
+            <p className="text-xs mt-2">Desenvolvido por Diego Carlos</p>
+        </div>
+    </div> );
+}
+ 
+export default Footer;
